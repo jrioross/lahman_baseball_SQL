5fad5fb565bf26a2ca553bf45c23dee20df88e7b
tipearso <tepearson@gmail.com>
2021-03-07 08:30:04 -0600

    Sunday Morning

--- data/lahman_baseball_tp.sql
+++ data/lahman_baseball_tp.sql
@@ -7,10 +7,15 @@ FROM TEAMS;
 --Question 2. 
 --Find the name and height of the shortest player in the database. 
 --How many games did he play in? What is the name of the team for which he played?
-SELECT p.namelast, p.namefirst, p.height, a.teamid, a.g_all
+SELECT p.namelast, p.namefirst, p.height, a.teamid, a.g_all, t.franchname
 FROM people AS p
 LEFT JOIN appearances AS a
 ON p.playerid = a.playerid
-ORDER BY P.height 
+LEFT JOIN teamsfranchises AS t
+ON a.teamid = t.franchid
+WHERE a.teamid = 'SLA'
+ORDER BY P.height;
+
+
 
 --Eddie Gaedel played in 1 game for the St. Louis Browns
