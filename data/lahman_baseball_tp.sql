24bbcba3eaf2e6000d4f611f50f7bdc822fb8572
tipearso <tepearson@gmail.com>
2021-03-07 10:07:26 -0600

    Sunday morning

--- data/lahman_baseball_tp.sql
+++ data/lahman_baseball_tp.sql
@@ -1,12 +1,13 @@
 --Question 1. 
 --What range of years for baseball games played does the provided database cover? 
---1871-2016
+	--1871-2016
 SELECT MIN(yearid), MAX(yearid) 
 FROM TEAMS;
 
 --Question 2. 
 --Find the name and height of the shortest player in the database. 
 --How many games did he play in? What is the name of the team for which he played?
+	--Eddie Gaedel played in 1 game for the St. Louis Browns
 SELECT p.namelast, p.namefirst, p.height, a.teamid, a.g_all, t.franchname
 FROM people AS p
 LEFT JOIN appearances AS a
@@ -16,6 +17,22 @@ ON a.teamid = t.franchid
 WHERE a.teamid = 'SLA'
 ORDER BY P.height;
 
+-- Question 4.
+--Find all players in the database who played at Vanderbilt University. 
+--Create a list showing each player’s first and last names as well as the total salary 
+--they earned in the major leagues. 
+--Sort this list in descending order by the total salary earned. 
+--Which Vanderbilt player earned the most money in the majors?
+	--David Price
 
+SELECT DISTINCT c.playerid, SUM(s.salary),p.namelast, p.namefirst, c.schoolid
+FROM collegeplaying as c
+JOIN people as p
+ON p.playerid = c.playerid
+JOIN salaries as s
+ON p.playerid = s.playerid
+WHERE c.schoolid LIKE '%vand%'
+GROUP BY c.playerid,p.namelast,p.namefirst,c.schoolid
+ORDER BY SUM(s.salary) DESC
 
---Eddie Gaedel played in 1 game for the St. Louis Browns
+ 
