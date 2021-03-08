c3ee706f34376c14db098af14dd9b4316d8f4678
tipearso <tepearson@gmail.com>
2021-03-08 11:23:37 -0600

    monday morning

--- data/lahman_baseball_tp.sql
+++ data/lahman_baseball_tp.sql
@@ -17,7 +17,7 @@ ON a.teamid = t.franchid
 WHERE a.teamid = 'SLA'
 ORDER BY P.height;
 
--- Question 4.
+-- Question 3.
 --Find all players in the database who played at Vanderbilt University. 
 --Create a list showing each player’s first and last names as well as the total salary 
 --they earned in the major leagues. 
@@ -25,14 +25,38 @@ ORDER BY P.height;
 --Which Vanderbilt player earned the most money in the majors?
 	--David Price
 
-SELECT DISTINCT c.playerid, SUM(s.salary),p.namelast, p.namefirst, c.schoolid
-FROM collegeplaying as c
-JOIN people as p
+SELECT DISTINCT c.playerid, SUM(s.salary), p.namelast, p.namefirst, c.schoolid
+FROM collegeplaying c
+JOIN people p
 ON p.playerid = c.playerid
-JOIN salaries as s
+JOIN salaries s
 ON p.playerid = s.playerid
 WHERE c.schoolid LIKE '%vand%'
 GROUP BY c.playerid,p.namelast,p.namefirst,c.schoolid
-ORDER BY SUM(s.salary) DESC
+ORDER BY SUM(s.salary) DESC;
 
- 
+--Question 4
+--Using the fielding table, group players into three groups based on their position: 
+--label players with position OF as "Outfield", those with position "SS", "1B", "2B", 
+--and "3B" as "Infield", and those with position "P" or "C" as "Battery". 
+--Determine the number of putouts made by each of these three groups in 2016
+SELECT DISTINCT sub.position fielding_group, SUM(sub.putouts) total_putouts
+FROM
+	(SELECT pos, SUM(po) AS putouts,
+	CASE WHEN pos = 'OF' THEN 'Outfield'
+	WHEN pos = 'SS' OR pos = '1B' OR pos = '2B' OR pos = '3B' THEN 'Infield'
+	WHEN pos = 'P' OR pos = 'C' THEN 'Battery' 
+	END AS position
+	FROM fielding
+	WHERE yearid = 2016
+	group by position,pos) as sub
+GROUP BY position;
+
+--Question 5.
+--Find the average number of strikeouts per game by decade since 1920. 
+--Round the numbers you report to 2 decimal places. Do the same for home runs per game. 
+--Do you see any trends?
+
+SELECT COUNT(so)
+FROM batting
+WHERE yearid > 1920;
