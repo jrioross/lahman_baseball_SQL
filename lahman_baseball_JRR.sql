cd3b99a0665818839d63aaee36e906816206d8ee
jrioross <j.rioross@gmail.com>
2021-03-09 21:32:49 -0600

    Complete through #10

--- lahman_baseball_JRR.sql
+++ lahman_baseball_JRR.sql
@@ -46,7 +46,9 @@ WHERE yearid = 1996
 GROUP BY pos_group;
 
 --Q5
-WITH pitching_grouped AS (
+
+--Strikeouts per game
+WITH teams_grouped AS (
 	SELECT yearid,
 			CASE WHEN yearid BETWEEN 1920 AND 1929 THEN '1920s'
 			WHEN yearid BETWEEN 1930 AND 1939 THEN '1930s'
@@ -59,11 +61,187 @@ WITH pitching_grouped AS (
 			WHEN yearid BETWEEN 2000 AND 2009 THEN '2000s'
 			WHEN yearid >= 2010 THEN '2010s' END AS decade,
 			g,
-			so
-	FROM pitching
+			so,
+			hr
+	FROM teams
 )
 SELECT decade,
-		SUM(so),
-		SUM(g)
-FROM pitching_grouped
+		SUM(so) AS total_strikeouts,
+		SUM(g) AS total_games,
+		SUM(hr) AS total_hr,
+		ROUND(2*CAST(SUM(so) AS numeric)/ SUM(g),2) AS so_per_game,
+		ROUND(2*CAST(SUM(hr) AS numeric)/ SUM(g),2) AS hr_per_game
+FROM teams_grouped
 GROUP BY decade
+ORDER BY decade;
+
+--Q6
+WITH b AS (
+	SELECT playerid,
+			yearid,
+			sb,
+			cs
+	FROM batting
+	WHERE (sb+cs) >= 20
+	AND yearid = 2016
+)
+SELECT b.playerid,
+		namefirst,
+		namelast,
+		ROUND(1.00*sb/(sb+cs) * 100, 2) AS sb_perc
+FROM b
+INNER JOIN people AS p
+USING (playerid)
+WHERE 1.00*sb/(sb+cs) = (SELECT MAX(1.00*sb/(sb+cs)) FROM b)
+
+--Q7
+
+WITH ws_champ AS (
+	SELECT yearid,
+			g,
+			teamid,
+			w
+	FROM teams
+	WHERE wswin = 'N'
+	AND yearid BETWEEN 1970 AND 2016
+	)
+SELECT yearid,
+		g,
+		teamid,
+		w
+FROM ws_champ
+WHERE w = (SELECT MAX(w) FROM ws_champ);
+--Max Wins: 2001 Seattle Mariners with 116 wins
+
+WITH ws_champ AS (
+	SELECT yearid,
+			g,
+			teamid,
+			w
+	FROM teams
+	WHERE wswin = 'Y'
+	AND yearid BETWEEN 1970 AND 2016
+	)
+SELECT yearid,
+		g,
+		teamid,
+		w
+FROM ws_champ
+WHERE w = (SELECT MIN(w) FROM ws_champ)
+--Min wins for shortened (lockout) season: 1981 LA Dodgers with 63 wins
+
+WITH ws_champ AS (
+	SELECT yearid,
+			g,
+			teamid,
+			w
+	FROM teams
+	WHERE wswin = 'Y'
+	AND yearid BETWEEN 1970 AND 2016
+	AND yearid <> 1981
+	)
+SELECT yearid,
+		g,
+		teamid,
+		w
+FROM ws_champ
+WHERE w = (SELECT MIN(w) FROM ws_champ)
+--Min wins excluding 1981: 2006 St. Louis Cardinals with 83 wins
+
+WITH max_ws_champ AS (
+	SELECT yearid,
+			MAX(w) AS max_w
+	FROM teams
+	WHERE yearid BETWEEN 1970 AND 2016
+	GROUP BY yearid
+	)
+SELECT SUM(CASE WHEN wswin = 'Y' THEN 1 ELSE 0 END) AS ct_max_is_champ,
+		AVG(CASE WHEN wswin = 'Y' THEN 1 ELSE 0 END) AS perc_max_is_champ
+FROM max_ws_champ AS m
+INNER JOIN teams AS t
+ON m.yearid = t.yearid AND m.max_w = t.w
+--Max is champ: 12 teams. Percent max is champ: 22.6%
+
+--Q8
+
+--Top 5 Attendance in 2016
+	SELECT franchname,
+			park_name,
+			attendance/games AS avg_att
+	FROM homegames
+	INNER JOIN parks
+	USING (park)
+	INNER JOIN teamsfranchises AS tf
+	ON homegames.team = tf.franchid
+	WHERE year = 2016 AND games > 10
+	ORDER BY attendance/games DESC
+	LIMIT 5
+
+--Bottom 5 Attendance in 2016
+	SELECT franchname,
+			park_name,
+			attendance/games AS avg_att
+	FROM homegames
+	INNER JOIN parks
+	USING (park)
+	INNER JOIN teamsfranchises AS tf
+	ON homegames.team = tf.franchid
+	WHERE year = 2016 AND games > 10
+	ORDER BY attendance/games
+	LIMIT 5
+	
+--Q9
+WITH NL_TSN AS (
+	SELECT playerid
+	FROM awardsmanagers AS aw
+	WHERE awardid LIKE 'TSN %'
+	AND lgid = 'NL'
+	),
+AL_TSN AS(
+	SELECT playerid
+	FROM awardsmanagers AS aw
+	WHERE awardid LIKE 'TSN %'
+	AND lgid = 'AL'
+)
+SELECT DISTINCT am.playerid,
+		p.namefirst,
+		p.namelast,
+		am.lgid,
+		am.yearid,
+		tf.franchname
+FROM awardsmanagers AS am
+LEFT JOIN managers AS m
+USING (playerid, yearid)
+LEFT JOIN teamsfranchises AS tf
+ON m.teamid = tf.franchid
+LEFT JOIN people AS p
+USING (playerid)
+WHERE playerid IN
+	(SELECT *
+	FROM NL_TSN
+	INTERSECT
+	SELECT *
+	FROM AL_TSN)
+ORDER BY namelast, yearid;
+
+--Open-Ended Questions-----------------------------------------------------------------------------------------
+
+--Q10: Select which colleges in TN have players with the most games in the MLB
+
+SELECT DISTINCT schoolname,
+		playerid, namefirst, namelast,
+		SUM(g_all) OVER(PARTITION BY playerid) AS g_total_player,
+		SUM(g_all) OVER(PARTITION BY schoolname) AS g_total_school
+FROM appearances AS a
+INNER JOIN people AS p
+USING (playerid)
+INNER JOIN collegeplaying AS cp
+USING (playerid)
+INNER JOIN schools
+USING (schoolid)
+WHERE schoolstate = 'TN'
+ORDER BY g_total_school DESC, g_total_player DESC;
+
+--Q11: Correlation between wins and team salary (after 2000)
+
+SELECT 
